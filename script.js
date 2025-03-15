85b84b49afc9688217f43713203a0b8dca256cb2
Reixx7 <reixx23and7@gmail.com>
2025-03-15 11:04:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+alert(`Sizda hozir random son chiqadi`)
+let number = (Math.round(Math.random()*100))
+alert(`Random son ${number}`)
+alert(`Biz random soni 2ga kopaytirmoqchi va bolmoqchimiz`)
+alert(`Random sonni 2ga kopaytirsa ${number*2} boladi \n 
+    Random sonni 2ga bo'lsa ${number/2} bo'ladi\n 
+    Random sonni 3ga bo'lsa qoldig'i ${number%3} bo'ladi`)
